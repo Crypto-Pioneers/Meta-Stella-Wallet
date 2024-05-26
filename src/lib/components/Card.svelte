2433fa3cbad29ee2a2ee1ae89498f05bb689ba00
codepert <trang.uixdev@gmail.com>
2024-05-27 06:31:22 +0900

    Define card style.

---
+++ src/lib/components/Card.svelte
@@ -0,0 +1,73 @@
+<style>
+  .card {
+    height: fit-content;
+    background: rgba(255, 255, 255, 0.2);
+    border: solid 1px rgba(255, 255, 255, 0.7);
+    border-radius: 5px;
+    padding: 2rem 2rem 1rem 2rem;
+    box-shadow: 0px 8px 20px rgba(0, 0, 0, 0.15);
+    flex-direction: column;
+    backdrop-filter: blur(20px);
+    z-index: 100;
+    transition: width 1s, height 350ms;
+  }
+
+  
+      .circle {
+        width: 300px;
+        height: 300px;
+        border-radius: 50%;
+        filter: blur(60px);
+      }
+
+      .circlebg {
+        display: flex;
+        position: absolute;
+        z-index: -2;
+        opacity: 0.7;
+      }
+      .circle-red {
+        background: red;
+        transform: translateX(40%);
+      }
+
+      .circle-blue {
+        background-color: blue;
+        transform: translateX(-40%);
+      }
+
+      .circle-green {
+        background-color: green;
+      }
+
+      .circle-yellow {
+        background-color: yellow;
+        transform: translateX(-80%);
+      }
+
+</style>
+<script>
+  export let title = '';
+  export let img='';
+  let imgDisplay = 'none'
+  $: {
+    if (img != '') {
+      imgDisplay = 'block'
+    }
+  }
+</script>
+
+<div class="circlebg">
+  <!-- <div class="circle circle-red"></div> -->
+  <!-- <div class="circle circle-green"></div>
+  <div class="circle circle-blue"></div> -->
+  <!-- <div class="circle circle-yellow"></div> -->
+</div>
+<div class="card">
+  <div class="">
+    <h5>{title}</h5>
+  </div>
+  <div>
+    <img src={img}  alt="img" style={`display:${imgDisplay}`} width="100%">
+  </div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,14 +2,16 @@
 	import Counter from './Counter.svelte';
 	import welcome from '$lib/images/svelte-welcome.webp';
 	import welcome_fallback from '$lib/images/svelte-welcome.png';
-</script>
+	import wallet_img from '$lib/images/wallet.png';
+	import Card from '$lib/components/Card.svelte';
 
+</script>
 <svelte:head>
 	<title>Home</title>
 	<meta name="description" content="Svelte demo app" />
 </svelte:head>
 
-<section>
+<!-- <section>
 	<h1>
 		<span class="welcome">
 			<picture>
@@ -26,8 +28,55 @@
 	</h2>
 
 	<Counter />
-</section>
+</section> -->
+<section>
+	<div class="flex gap-3">
+		<div class="w-[70%]">
+			<div class="card">
+				<div class="my-3">
+					<h5 class="font-semibold">Wallet</h5>
+				</div>
+				<div>
+					<img src={wallet_img}  alt="img"  width="100%">
+				</div>
+			</div>
+		</div>
+		<div class="w-[30%]">
+			
+			<div class="card">
+				<div class="my-3">
+					<h5 class="font-semibold">Wallet</h5>
+				</div>
+				<div>
+					<p>Now here’s a cool one. Hover and the card’s contents slide away, making room for a fresh color and a logo. Smooth.</p>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="flex gap-3 mt-3">
+		<div class="w-[30%]">
+			<div class="card">
+				<div class="my-3">
+					<h5 class="font-semibold">Wallet</h5>
+				</div>
+				<div>
+					<p>Now here’s a cool one. Hover and the card’s contents slide away, making room for a fresh color and a logo. Smooth.</p>
+				</div>
+			</div>
+		</div>
+		<div class="w-[70%]">
+			<div class="card">
+				<div class="my-3">
+					<h5 class="font-semibold">Wallet</h5>
+				</div>
+				<div>
+					<p>Now here’s a cool one. Hover and the card’s contents slide away, making room for a fresh color and a logo. Smooth.</p>
+				</div>
+			</div>
+		</div>
 
+	</div>
+</section>
 <style>
 	section {
 		display: flex;
@@ -37,23 +86,47 @@
 		flex: 0.6;
 	}
 
-	h1 {
-		width: 100%;
-	}
-
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
+	.card {
+    /* height: fit-content; */
 		height: 100%;
-		top: 0;
-		display: block;
+    /* background: rgba(255, 255, 255, 0.2); */
+    border: solid 1px rgba(255, 255, 255, 0.7);
+    border-radius: 15px;
+    padding: 2rem 2rem 1rem 2rem;
+    box-shadow: 0px 8px 20px rgba(0, 0, 0, 0.15);
+    flex-direction: column;
+    backdrop-filter: blur(20px);
+    z-index: 100;
+    transition: width 1s, height 350ms;
+		background-image: linear-gradient(135deg, #ffffff, #f0f0f0, #ffffff);
+  }
+	.circle {
+		width: 300px;
+		height: 300px;
+		border-radius: 50%;
+		filter: blur(60px);
 	}
+
+	.circlebg {
+		display: flex;
+		position: absolute;
+		z-index: -2;
+		opacity: 0.7;
+	}
+	.circle-red {
+		background: red;
+		transform: translateX(40%);
+	}
+	.circle-blue {
+		background-color: blue;
+		transform: translateX(-40%);
+	}
+	.circle-green {
+		background-color: green;
+	}
+	.circle-yellow {
+		background-color: yellow;
+		transform: translateX(-80%);
+	}
+
 </style>
